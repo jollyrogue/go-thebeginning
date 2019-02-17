7b0abd50e1bb57ff16ee8dcfff2c671945657ed2
Ryan Quinn <rxqy@dangertoaster.com>
2019-02-17 17:08:21 -0600

    Added multiple return vars to the printNames function.
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -12,11 +12,29 @@ func main() {
 	fmt.Printf("All Arguments: (%T) %+v\n", argsAll, argsAll)
 	fmt.Printf("Arguments: (%T) %+v\n", argsPartial, argsPartial)
 
-	printNames(argsPartial)
+	charCount, longStrings := printNames(argsPartial)
+
+	fmt.Printf("Longest Strings: %v\n", longStrings)
+	fmt.Printf("Number of Characters in strings: %d\n", charCount)
 }
 
-func printNames(nameList []string) {
+func printNames(nameList []string) (characterCount uint, longestString []string) {
+	maxLength := 0
+
 	for index, name := range nameList {
-		fmt.Printf("Hello, %s.\t(%d)\n", name, index)
+		fmt.Printf("Hello, %s.\t(idx: %d, charcount: %d)\n", name, index, len(name))
+		characterCount += uint(len(name))
+
+		if len(name) > maxLength {
+			maxLength = len(name)
+		}
 	}
+
+	for _, name := range nameList {
+		if len(name) == maxLength {
+			longestString = append(longestString, name)
+		}
+	}
+
+	return
 }
