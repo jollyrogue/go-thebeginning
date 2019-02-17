6b510c3dce2c43486cf1b9b25a89342de48f0717
Ryan Quinn <rxqy@dangertoaster.com>
2019-02-17 17:56:58 -0600

    Flags work and there is color output.
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+var printLongestNames bool
+
+func init() {
+	flag.BoolVar(&printLongestNames, "l", false, "Prints the longest names passed in the arguments.")
+}
+
 func main() {
 	argsAll := os.Args
 	argsPartial := os.Args[1:]
+
+	flag.Parse()
 
 	fmt.Printf("All Arguments: (%T) %+v\n", argsAll, argsAll)
 	fmt.Printf("Arguments: (%T) %+v\n", argsPartial, argsPartial)
 
 	charCount, longStrings := printNames(argsPartial)
 
-	fmt.Printf("Longest Strings: %v\n", longStrings)
+	if printLongestNames == true {
+		fmt.Printf("Longest Strings: %v\n", longStrings)
+	} else {
+		fmt.Println("\033[38;5;196m>>>\033[0m    \033[38;5;21;48;5;118mPrinting of longest names has been turned off!\033[0m    \033[38;5;196m<<<\033[0m")
+	}
 	fmt.Printf("Number of Characters in strings: %d\n", charCount)
 }
 
