b374dd1d78313cf7f52e77b6edc56f6b3e5d9796
Ryan Quinn <rxqy@dangertoaster.com>
2019-02-17 13:36:25 -0600

    Added argument parsing.
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	argsAll := os.Args
+	argsActual := os.Args[1:]
+
+	fmt.Printf("All Arguments: %+v\n", argsAll)
+	fmt.Printf("Arguments: %+v\n", argsActual)
 	fmt.Println("Hello.")
 }
